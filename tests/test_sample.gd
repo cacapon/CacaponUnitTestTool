d33ab87db916edf64ae332fc856f76a94d87c69c
Cacapon Project <cacapon.project@gmail.com>
2024-03-10 18:22:18 +0900

    サンプルのテスト追加

---
+++ tests/test_sample.gd
@@ -0,0 +1,22 @@
+extends CacaponUnitTest
+
+func test_eq_ok() -> Dictionary:
+	var a := 1
+	var b := 1
+	return assert_eq(a,b)
+
+#func test_eq_ng() -> Dictionary:
+	#var a := 1
+	#var b := 2
+	#return assert_eq(a,b)
+
+func test_not_eq_ok() -> Dictionary:
+	var a := "foo"
+	var b := "bar"
+	return assert_not_eq(a,b)
+
+#func test__not_eq_ng() -> Dictionary:
+	#var a := "foo"
+	#var b := "foo"
+	#return assert_not_eq(a,b)
+
